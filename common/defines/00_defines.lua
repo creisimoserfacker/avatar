53bfe2bfb57b109b50a2ac1526ecb23f477a103c
Paul <paul.kammers12@gmail.com>
2021-04-18 01:31:19 +0200

    the dragons must die

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -2468,7 +2468,7 @@ NFocus = {
 },
 
 NOperatives = {
-	AGENCY_CREATION_DAYS = 1,--180,						-- Number of days needed to create an intelligence agency
+	AGENCY_CREATION_DAYS = 180,						-- Number of days needed to create an intelligence agency
 	AGENCY_UPGRADE_DAYS = 60,						-- Number of days needed to upgrade an intelligence agency
 	AGENCY_CREATION_FACTORIES = 5,					-- Number of factories used to create an intelligence agency
 	AGENCY_AI_BASE_NUM_FACTORIES = 25.0,				-- Used by AI to pace the upgrades. Formula : if( AGENCY_AI_BASE_NUM_FACTORIES <= num_civ_factories - num_upgrades * AGENCY_AI_PER_UPGRADE_FACTORIES )
